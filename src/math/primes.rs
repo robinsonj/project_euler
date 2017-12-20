b42980ce784aeea2fc4098d2f190c7f0d9647056
Joseph L. Robinson <robinsonjosephlewis@gmail.com>
2017-12-19 18:03:38 -0800

    Add prime factorization to lib

--- src/math/primes.rs
+++ src/math/primes.rs
@@ -1,53 +1,109 @@
-pub fn is_prime(p: u64) -> bool {
-  factors(p) == vec![p]
+use std::collections::HashMap;
+
+#[derive(Debug)]
+pub struct Factors {
+  num: u64,
+  factors: HashMap<u64, u32>,
+  factorized: bool
 }
 
-/// Generate a vector of the prime factors of a given integer.
-///
-/// # Arguments
-///
-/// * `f` - An unsigned integer to be factorized.
-///
-/// # Examples
-///
-/// ```
-/// use euler_lib::math::primes::factors;
-/// assert_eq!(factors(10), vec![2, 5]);
-/// ```
-pub fn factors(mut f: u64) -> Vec<u64> {
-  let mut primes: Vec<u64> = Vec::new();
-  let mut candidate: u64 = 2;
-
-  while f > 1 {
-    while f % candidate == 0 {
-      primes.push(candidate);
-      f /= candidate;
+impl Factors {
+  pub fn new(num: u64) -> Factors {
+    Factors {
+      num: num,
+      factors: HashMap::new(),
+      factorized: false
     }
-
-    candidate += 1;
   }
 
-  primes
+  /// Generate a vector of the prime factors of a given integer.
+  pub fn factorize(&mut self) {
+    let mut f: u64 = self.num;
+    let mut candidate: u64 = 2;
+
+    while f > 1 {
+      while f % candidate == 0 {
+        self.add(candidate);
+        f /= candidate;
+      }
+
+      candidate += 1;
+    }
+
+    self.factorized = true
+  }
+
+  fn add(&mut self, i: u64) {
+    let count = self.factors.entry(i).or_insert(0);
+    *count += 1;
+  }
+
+  pub fn to_int(&mut self) -> u64 {
+    if !&self.factorized {
+      &self.factorize();
+    }
+
+    let mut num: u64 = 1;
+
+    for (i, c) in &self.factors {
+      num *= i.pow(*c);
+    }
+
+    num
+  }
+
+  pub fn to_vec(&mut self) -> Vec<u64> {
+    if !&self.factorized {
+      &self.factorize();
+    }
+
+    let mut factors: Vec<u64> = vec![];
+
+    for (i, c) in &self.factors {
+      for _ in 0..*c {
+        factors.push(*i)
+      }
+    }
+
+    factors
+  }
+
+  pub fn is_prime(&self) -> bool {
+    self.factorized && self.factors.iter().len() == 1
+  }
+}
+
+pub fn is_prime(p: u64) -> bool {
+  let mut factors = Factors::new(p);
+
+  factors.to_vec() == vec![p]
 }
 
 #[cfg(test)]
 mod tests {
-  use super::*;
-
   #[test]
-  fn test_is_prime() {
-    assert_eq!(is_prime(894119), true);
-    assert_eq!(is_prime(894120), false);
+  fn is_prime() {
+    assert_eq!(super::is_prime(894119), true);
+    assert_eq!(super::is_prime(894120), false);
   }
 
   #[test]
-  fn test_factors() {
-    assert_eq!(factors(6), vec![2, 3]);
-    assert_eq!(factors(8), vec![2, 2, 2]);
-    assert_eq!(factors(9), vec![3, 3]);
-    assert_eq!(factors(29), vec![29]);
-    assert_eq!(factors(100), vec![2, 2, 5, 5]);
-    assert_eq!(factors(901255), vec![5, 17, 23, 461]);
-    assert_eq!(factors(93819012551), vec![11, 9539, 894119]);
+  fn factors_struct_new() {
+    let num = 100;
+    let f = super::Factors::new(num);
+
+    assert_eq!(f.factorized, false);
+    assert_eq!(f.num, num);
+    assert_eq!(f.factors.is_empty(), true);
+  }
+
+  #[test]
+  fn factors_struct() {
+    let mut factors = super::Factors::new(24);
+    let mut f_vec = factors.to_vec();
+    f_vec.sort();
+
+    assert_eq!(factors.to_int(), 24);
+    assert_eq!(f_vec, vec![2, 2, 2, 3]);
   }
 }
