ecd0fe48300a597cc0981c557fdddf64edcbd34f
Joseph L. Robinson <robinsonjosephlewis@gmail.com>
2018-02-28 14:13:40 -0800

    Benchmark p012
    The algorithm for solving p012 can be modified as follows:
    
    ```
    let mut n: u64 = 1;
    
    loop {
      let t: u64 = gauss_sum(n);
      let sum: u64;
    
      if n % 2 == 0 {
        sum = primes::Set::of(n / 2).num_divisors() *
          primes::Set::of(n + 1).num_divisors()
      } else {
        sum = primes::Set::of(n).num_divisors() *
          primes::Set::of((n + 1) / 2).num_divisors()
      }
    
      if sum >= min { return t }
    
      n += 1;
    }
    ```
    
    The above attempts to achieve a speed increase in solving for the
    solution but instead results in an 11% slowdown. Code is recorded here
    for posterity.

--- src/bin/p012.rs
+++ src/bin/p012.rs
@@ -1,5 +1,8 @@
 //! [Problem 012](https://projecteuler.net/problem=12)
 
+#![feature(test)]
+extern crate test;
+
 #[macro_use(problem)]
 extern crate euler_lib;
 
@@ -17,6 +20,8 @@ problem!(&76_576_500.to_string(), solve);
 
 #[cfg(test)]
 mod tests {
+  use test::Bencher;
+
   #[test]
   fn p012_example() {
     assert_eq!(28, super::compute(5));
@@ -25,5 +30,10 @@ mod tests {
   #[test]
   fn p012_solution() {
     assert_eq!(76_576_500, super::compute(500));
+  }
+
+  #[bench]
+  fn bench_p012_solution(b: &mut Bencher) {
+    b.iter(|| super::compute(500));
   }
 }
